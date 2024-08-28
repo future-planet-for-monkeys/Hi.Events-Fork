d45d546bd5e6d0be55a2e1f60c3e370bb5cf59c7
carlosalaniz <YOUR_EMAIL>
2024-08-27 22:19:16 -0500

    fix csr build

--- frontend/src/utilites/config.ts
+++ frontend/src/utilites/config.ts
@@ -2,6 +2,13 @@ import { ConfigKeys } from "../types.ts";
 import { isSsr } from "./helpers.ts";
 import process from "process";
 
+const BuildTimeVars: { [key: string]: string } = {
+    'VITE_API_URL_CLIENT': import.meta.env.VITE_API_URL_CLIENT,
+    'VITE_API_URL_SERVER': import.meta.env.VITE_API_URL_SERVER,
+    'VITE_STRIPE_PUBLISHABLE_KEY': import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY,
+    'VITE_I_HAVE_PURCHASED_A_LICENCE': import.meta.env.VITE_I_HAVE_PURCHASED_A_LICENCE,
+    'VITE_FRONTEND_URL': import.meta.env.VITE_FRONTEND_URL,
+};
 export const getConfig = (key: ConfigKeys): string | undefined => {
     if (isSsr()) {
         const serverEnv = typeof process !== "undefined" && process.env ? process.env : {};
@@ -9,5 +16,5 @@ export const getConfig = (key: ConfigKeys): string | undefined => {
     }
 
     const clientEnv = typeof window !== "undefined" && window.hievents ? window.hievents : {};
-    return clientEnv[key];
+    return BuildTimeVars[key] || clientEnv[key];
 };
